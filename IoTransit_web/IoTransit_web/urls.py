f1a4fc197e1f74c7db21151dc7e93859fa3b945f
lala341 <li.forero@uniandes.edu.co>
2019-05-15 12:06:36 -0500

    a

--- IoTransit_web/IoTransit_web/urls.py
+++ IoTransit_web/IoTransit_web/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import patterns, path, include
-
+from django.urls import  path, include
+from django.conf.urls import patterns
 
 urlpatterns = patterns('',
     url(r'admin/', admin.site.urls),
